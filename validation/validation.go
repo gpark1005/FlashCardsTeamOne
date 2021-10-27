6514de2780d801922f56d7acadfbe4a10a95b7b7
Austin Nicholson <Anicholson@shipt.com>
2021-10-27 11:23:46 -0500

    add comment's to validation

--- validation/validation.go
+++ validation/validation.go
@@ -13,6 +13,8 @@ type dbVal struct {
 	Flashcards []map[string]interface{}
 }
 
+/* This function compares the questions map from the incoming data to
+what is stored in the db to see if card is already saved */
 func ValidateMatching(card cards.Matching, filename string) error {
 
 	Db := dbVal{}
@@ -27,6 +29,9 @@ func ValidateMatching(card cards.Matching, filename string) error {
 		return err
 	}
 
+	/* This loop with the help of the if ok statment, allows us to access the questions map stored on
+	each matching card saved in the db. Since we are comparing two maps, reflect.DeepEqual will return
+	true if they match or false if they do not. The bool is stored in value eq. */
 	for _, val := range Db.Flashcards {
 		if cQ, ok := val["questions"]; ok {
 			eq := reflect.DeepEqual(card.Questions, cQ)
